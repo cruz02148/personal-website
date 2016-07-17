ec72fb1fbecef3e49e8f6e5dcd699d78e226b768
cruz02148 <cruz02148@gmail.com>
2016-07-17 12:31:04 -0400

    Initial Commit
    Begin webpck configuration setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'client/public');
+const APP_DIR = path.resolve(__dirname, 'client/src');
+
+const config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: APP_DIR,
+        loaders: ['babel?cacheDirectory'],
+        loader: 'babel'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
+
+module.exports = config;
