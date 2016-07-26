6e8f0e60cc0db336d0277a2098dd56ea8d82f6c8
cruz02148 <cruz02148@gmail.com>
2016-07-26 02:21:21 -0400

    Remove config file in server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const emailAccount = require('../config');
 // const webpack = require('webpack');
 // const config = require('../webpack.config.js');
 // const webpackDevMiddleware = require('webpack-dev-middleware');
